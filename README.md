7563e73c5ee7415ae054c8e570494d16123eddea
dyrnq <dyrnq@qq.com>
2021-02-27 18:31:41 +0800

    1st push

---
+++ README.md
@@ -0,0 +1,9 @@
+# pid1-detecting
+
+This is a userful script detecting which system manager is running on Linux System!
+
+# Quickstart
+
+```bash
+curl -fsSL https://cdn.jsdelivr.net/gh/dyrnq/pid1-detecting@main/pid1-detecting.sh | bash -s pid1-detecting
+```
---
+++ pid1-detecting.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+comm="$(cat /proc/1/comm)"
+
+if [[ "$comm" =~ systemd ]]; then
+    init="systemd"
+elif [[ "$comm" =~ epoch ]]; then
+    init="Epoch"
+elif [[ "$comm" =~ runit ]]; then
+    init="runit"    
+fi
+
+if [ -z "$init" ]; then
+    if [[ $(/sbin/init --version) =~ upstart ]]; then
+        init="Upstart"
+    elif command -v "launchctl" > /dev/null 2>&1; then
+        init="launchd";
+    elif [ -f "/etc/inittab" ]; then
+        init="SysVinit";
+    elif [ -f "/etc/ttys" ]; then
+        init="init (BSD)";
+    fi
+fi
+
+if ls /run/*openrc* > /dev/null 2>&1; then
+    init="OpenRC";
+fi
+echo "$init"
